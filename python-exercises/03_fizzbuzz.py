b89ce1ba7117e7f31d18449846fb8e92f38aaf29
Prochainne <krish.sujit@gmail.com>
2026-06-10 20:59:22 +0400

    Added docstring for FizzBuzz

--- python-exercises/03_fizzbuzz.py
+++ python-exercises/03_fizzbuzz.py
@@ -1,3 +1,6 @@
+"""Write a program that prints the numbers from 1 to n. But for 
+multiples of fizz print "Fizz" instead of the number and for the multiples of 
+buzz print "Buzz". For numbers which are multiples of both fizz and buzz print "FizzBuzz"."""
 def fizzbuzz(n,fizz,buzz):
     for i in range(1, n+1):
         if i % (fizz*buzz) == 0:
